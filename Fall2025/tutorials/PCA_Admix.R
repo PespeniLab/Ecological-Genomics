e7b51bb1d1de9988c933346a96ec8b70746f4fa7
Stephen Keller <srkeller@uvm.edu>
2025-09-25 14:33:05 -0400

    update tutorial 5

--- Fall2025/tutorials/PCA_Admix.R
+++ Fall2025/tutorials/PCA_Admix.R
@@ -1,6 +1,7 @@
 library(ggplot2) # plotting
 library(ggpubr) # plotting
 
+
 setwd("~/Downloads") # set the path to where you saved the pcANGSD results on your laptop
 
 ## First, let's work on the genetic PCA:
@@ -25,26 +26,31 @@ barplot(var,
 
 names <- read.table("RSBS_bam.list")
 names <- unlist(strsplit(basename(as.character(names[,1])), split = ".sorted.rmdup.bam"))
-split = strsplit(names[,1], "_")
-#pops <- data.frame(names[1:95], do.call(rbind, split[1:95]))
-pops = names
-
+split = strsplit(names, "_")
+pops <- data.frame(names[1:113], do.call(rbind, split[1:113]))
 names(pops) = c("Ind", "Pop", "Row", "Col")
 
-## A quick and humble PCA plot:
-
-plot(PCA$vectors[,1:2],
-     col=as.factor(pops[,2]),
-     xlab="PC1",ylab="PC2", 
-     main="Genetic PCA")
-
-## A more beautiful PCA plot using ggplot :)
+## Plot the PCA using ggplot
 
 data=as.data.frame(PCA$vectors)
 data=data[,c(1:3)]
 data= cbind(data, pops)
 
-cols=c("#377eB8","#EE9B00","#0A9396","#94D2BD","#FFCB69","#005f73","#E26D5C","#AE2012", "#6d597a", "#7EA16B","#d4e09b", "gray70")
+# A simple ggplot:
+
+ggscatter(data, x = "V1", y = "V2",
+          color = "Pop",
+          mean.point = TRUE,
+          star.plot = TRUE,
+          main="Red spruce-Black spruce genetic PCA") +
+  theme_bw(base_size = 13, base_family = "Times") +
+  labs(x = paste0("PC1: (",var[1]*100,"%)"), y = paste0("PC2: (",var[2]*100,"%)"))
+
+
+
+# A more customized plot
+
+cols=c("#377eB8","#EE9B00","#0A9396","#94D2BD","#FFCB69","#005f73","#E26D5C","#AE2012", "#6d597a", "#7EA16B","#d4e09b", "gray70", "black", "darkgray", "purple")
 
 ggscatter(data, x = "V1", y = "V2",
           color = "Pop",
@@ -64,11 +70,12 @@ ggscatter(data, x = "V1", y = "V2",
 
 
 
+
 ## Next, we can look at the admixture clustering:
 
 # import the ancestry scores (these are the .Q files)
 
-q <- read.table("allRS_poly.admix.2.Q", sep=" ", header=F)
+q <- read.table("RSBS_poly.admix.2.Q", sep=" ", header=F)
 
 K=dim(q)[2] #Find the level of K modeled
 
@@ -76,10 +83,13 @@ K=dim(q)[2] #Find the level of K modeled
 ord<-order(pops[,2])
 
 # make the plot:
+pdf("~/Downloads/RSBS_admix_K2.pdf", width=8, height=4)
 barplot(t(q)[,ord],
-        col=cols[1:K],
+        #col=cols[1:K],
+        col=c("black","red"),
         space=0,border=NA,
         xlab="Populations",ylab="Admixture proportions",
-        main=paste0("Red spruce K=",K))
-text(tapply(1:nrow(pops),pops[ord,2],mean),-0.05,unique(pops[ord,2]),xpd=T)
+        main=paste0("Red Spruce-Black Spruce Admixture: K=",K))
+text(tapply(1:nrow(pops),pops[ord,2],mean),-0.05,unique(pops[ord,2]),xpd=T,cex=0.75,srt=45)
 abline(v=cumsum(sapply(unique(pops[ord,2]),function(x){sum(pops[ord,2]==x)})),col=1,lwd=1.2)
+dev.off()
